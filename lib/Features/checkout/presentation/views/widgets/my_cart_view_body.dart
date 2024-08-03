1c1a025e5c986da59184ad71cae15d88f06c1ddc
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-08-03 23:38:46 +0300

    views body

---
+++ lib/Features/checkout/presentation/views/widgets/my_cart_view_body.dart
@@ -0,0 +1,220 @@
+import 'package:checkout/Features/checkout/presentation/views/widgets/cart_info_item.dart';
+import 'package:checkout/Features/checkout/presentation/views/widgets/total_price_widget.dart';
+import 'package:checkout/core/widgets/custom_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class MyCartViewBody extends StatelessWidget {
+  const MyCartViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 18,
+          ),
+          const Expanded(child: CustomBasket()),
+          const SizedBox(
+            height: 25,
+          ),
+          const OrderInfoItem(
+            title: 'Order Subtotal',
+            value: r'42.97$',
+          ),
+          const SizedBox(
+            height: 3,
+          ),
+          const OrderInfoItem(
+            title: 'Discount',
+            value: r'0$',
+          ),
+          const SizedBox(
+            height: 3,
+          ),
+          const OrderInfoItem(
+            title: 'Shipping',
+            value: r'8$',
+          ),
+          const Divider(
+            thickness: 2,
+            height: 34,
+            color: Color(0xffC7C7C7),
+          ),
+          const TotalPrice(title: 'Total', value: r'$50.97'),
+          const SizedBox(
+            height: 16,
+          ),
+          CustomButton(
+            text: 'Complete Payment',
+            onTap: () {
+              // Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+              //   return const PaymentDetailsView();
+              // }));
+
+              // showModalBottomSheet(
+              //     context: context,
+              //     shape: RoundedRectangleBorder(
+              //         borderRadius: BorderRadius.circular(16)),
+              //     builder: (context) {
+              //       return BlocProvider(
+              //         create: (context) => PaymentCubit(CheckoutRepoImpl()),
+              //         child: const PaymentMethodsBottomSheet(),
+              //       );
+              //     });
+            },
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomBasket extends StatelessWidget {
+  const CustomBasket({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Image.asset('assets/images/basket_image.png'),
+        Positioned(
+          top: 10,
+          right: -30,
+          child: Stack(
+            children: [
+              SvgPicture.asset('assets/images/totalprice.svg'),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 12),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 18.0),
+                      child: Text(
+                        """Kinetic Sand Dino \nDig Playset""",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(18),
+                      child: Row(
+                        children: [
+                          Container(
+                            decoration: const BoxDecoration(
+                              color: Colors.white,
+                              border: Border(
+                                  top: BorderSide(
+                                    width: .5,
+                                  ),
+                                  bottom: BorderSide(
+                                    width: .5,
+                                  ),
+                                  left: BorderSide(
+                                    width: .5,
+                                  ),
+                                  right: BorderSide(
+                                    width: .5,
+                                  )),
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(5),
+                                bottomLeft: Radius.circular(5),
+                              ),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: IconButton(
+                                  padding: EdgeInsets.zero,
+                                  constraints: const BoxConstraints(),
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                    Icons.remove,
+                                    size: 18,
+                                  )),
+                            ),
+                          ),
+                          Container(
+                            decoration: const BoxDecoration(
+                              color: Colors.white,
+                              border: Border(
+                                top: BorderSide(
+                                  width: .5,
+                                ),
+                                bottom: BorderSide(
+                                  width: .5,
+                                ),
+                              ),
+                            ),
+                            child: const Padding(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 7.0, horizontal: 12),
+                              child: Text(
+                                '1',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            decoration: const BoxDecoration(
+                              color: Colors.white,
+                              border: Border(
+                                  top: BorderSide(
+                                    width: .5,
+                                  ),
+                                  bottom: BorderSide(
+                                    width: .5,
+                                  ),
+                                  left: BorderSide(
+                                    width: .5,
+                                  ),
+                                  right: BorderSide(
+                                    width: .5,
+                                  )),
+                              borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(5),
+                                bottomRight: Radius.circular(5),
+                              ),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: IconButton(
+                                  padding: EdgeInsets.zero,
+                                  constraints: const BoxConstraints(),
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                    Icons.add,
+                                    size: 18,
+                                  )),
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Text(
+                              r"$19.99",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 18),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
